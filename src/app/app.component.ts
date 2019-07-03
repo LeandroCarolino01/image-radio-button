d5761f8232481423130b1e7a10b98576f589eadc
Andrey Kolybelnikov <andrey@fastforward.ai>
2019-07-03 14:40:23 +0200

    bugfix:selected image preview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,30 @@
 import { Component } from '@angular/core';
- 
-import {Item} from '../app/item';
-import {ITEMS} from '../app/mock-data';
- 
+
+import { Item } from '../app/item';
+import { ITEMS } from '../app/mock-data';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
-  radioSel:any;
-  radioSelected:string;
-  radioSelectedString:string;
+  radioSel: any;
+  radioSelected: string;
+  itemSelected: Item;
   itemsList: Item[] = ITEMS;
- 
-    
-    constructor() {
-      this.itemsList = ITEMS;
-      this.radioSelected = "assets/imgs/space3.jpg";
-      this.getSelecteditem();
-     }
- 
-    getSelecteditem(){
-      this.radioSel = ITEMS.find(Item => Item.img === this.radioSelected);
-      this.radioSelectedString = JSON.stringify(this.radioSel);
-      // this.radioSelectedString = this.radioSel;
-      console.log(this.radioSel);
-    }
- 
-    onItemChange(item){
-      this.getSelecteditem();
-    }
- 
+
+  constructor() {
+    this.itemsList = ITEMS;
+    this.radioSelected = this.itemsList[0].img;
+    this.getSelecteditem();
+  }
+
+  getSelecteditem() {
+    this.itemSelected = ITEMS.find(i => i.img === this.radioSelected);
+  }
+
+  onItemChange(item: Item): void {
+    this.itemSelected = item;
+  }
 }
