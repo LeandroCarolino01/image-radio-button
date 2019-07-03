9bdccd45475b4c3ad9ba73e7f79bfd8047159171
Leandro Carolino Dos Santos <leandro@watermelon.co>
2019-07-03 14:07:41 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
-
+ 
+import {Item} from '../app/item';
+import {ITEMS} from '../app/mock-data';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'imgRadio';
+
+  radioSel:any;
+  radioSelected:string;
+  radioSelectedString:string;
+  itemsList: Item[] = ITEMS;
+ 
+    
+    constructor() {
+      this.itemsList = ITEMS;
+      this.radioSelected = "assets/imgs/space3.jpg";
+      this.getSelecteditem();
+     }
+ 
+    getSelecteditem(){
+      this.radioSel = ITEMS.find(Item => Item.img === this.radioSelected);
+      this.radioSelectedString = JSON.stringify(this.radioSel);
+      // this.radioSelectedString = this.radioSel;
+      console.log(this.radioSel);
+    }
+ 
+    onItemChange(item){
+      this.getSelecteditem();
+    }
+ 
 }
